067d375c9e628e3f190f3f6e10e69ad616102556
Janaina Cazuza <janainamartinscazuza@gmail.com>
2024-12-22 19:44:01 -0300

    Updating Course praticing python

--- python_courses/alura_praticing_loops/praticing_loops.py
+++ python_courses/alura_praticing_loops/praticing_loops.py
@@ -39,3 +39,47 @@ for livro in livros:
 
 # Exercise 6 :
 estoque = 5
+while estoque > 0:
+    print(f"venda realizada, restam {estoque} produtos")
+    estoque =-1
+
+print("Estoque esgotado")
+
+#Exercise 7:
+
+for numero in range (10, 0, -1):
+    if numero % 2 == 0:
+        print(f"Faltam apenas {numero} segundos - Não perca essa oportunidade!")
+    else:
+        print(f"A contagem continua: {numero} segundos restantes.")
+print("Aproveite a promoção agora!")
+
+#Exercise 8:
+
+livros = [
+    {"nome": "1984", "estoque": 5},
+    {"nome": "Dom Casmurro", "estoque": 0},
+    {"nome": "O Pequeno Príncipe", "estoque": 3},
+    {"nome": "O Hobbit", "estoque": 0},
+    {"nome": "Orgulho e Preconceito", "estoque": 2}
+]
+
+for livro in livros:
+    if livro["estoque"] > 0:
+        print (f"Livro disponível: {livro["nome"]}")
+        
+
+#Exercise 9:
+while True:
+    user = input("Digite seu nome: ")
+    password = input("Digite sua senha: ")
+
+    if len(user) < 5: 
+        print("O nome de usuário deve ter no mínimo 5 caracteres.")
+        continue
+    if len(password) < 8:
+        print("A senha deve ter no mínimo 8 caracteres.")
+        continue
+
+    print("Usuário cadastrado com sucesso!")
+    break
